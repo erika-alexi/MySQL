89c5cbbe87115b41d0b574f01fca1655649dbc9d
erika-alexi <143255531+erika-alexi@users.noreply.github.com>
2026-07-20 18:07:28 +0800

    initial commit

---
+++ 3. Where Statement.sql
@@ -0,0 +1,54 @@
+-- WHERE Clause
+
+SELECT *
+FROM employee_salary
+WHERE first_name = 'Leslie'
+;
+
+SELECT *
+FROM employee_salary
+WHERE salary <= 50000
+;
+
+SELECT *
+FROM employee_demographics
+WHERE birth_date > '1985-01-01'
+;
+
+
+-- AND OR NOT -- Logical Operators
+SELECT *
+FROM employee_demographics
+WHERE birth_date > '1985-01-01'
+OR NOT gender = 'male'
+;
+
+
+/*
+SELECT *
+FROM employee_demographics
+WHERE (first_name = 'Leslie' AND age = 44) OR age > 55
+*/
+
+
+-- LIKE Statement
+-- % and _
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE 'a___%'
+-- WHERE last_name LIKE '%e'
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
